db3c884ca2e2ab9cfe6b46e226a10d0ae75ba5eb
Kanniyappa Ranjith M <kanniyapparanjithm987@gmail.com>
2026-08-14 16:45:53 +0530

    package and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
@@ -7,6 +8,17 @@ const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected successfully')
+  })
+  .catch((error) => {
+    console.error('MongoDB connection failed:')
+    console.error(error)
+  })
 
 // Test route
 app.get('/', (req, res) => {
